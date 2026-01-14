998380b35e17d08fa9c8ded666031ab9f7b79439
Hanamant K <hanamant.k@enhancesys.com>
2026-01-14 15:16:33 +0530

    commite delete changes

--- Selenium/CucumberBDD/src/test/java/CucumberTestNg/TestNgRunner.java
+++ Selenium/CucumberBDD/src/test/java/CucumberTestNg/TestNgRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 public class TestNgRunner {
 	
-@CucumberOptions(features = "src/test/java/FeatureFile", glue = "StepDefinition", monochrome = true, tags="@placeorder",
+@CucumberOptions(features = "src/test/java/FeatureFile", glue = "StepDefinition", monochrome = true, tags="@Lpo_01",
 			
 
 		plugin = { "html:target/cucmber.html", "json:target/cucumber.json"})
--- Selenium/FrameworkScratchBatch2/src/main/java/Constants/ConstantData.java
+++
@@ -1,8 +0,0 @@
-package Constants;
-
-public class ConstantData {
-	
-	
-	public static final String ExcelPath="src\\test\\java\\TestData\\TestData25thJune.xlsx";
-    public static final String PropertyFilePath ="src\\main\\java\\Global.properties";
-}
