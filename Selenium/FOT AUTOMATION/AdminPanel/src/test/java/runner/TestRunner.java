b5142ed677778c406f7cfeee5eb38361672553eb
Akshata Meti <akshata.meti@enhancesys.com>
2026-02-02 14:22:58 +0530

    commiting just formated code of main runner class

--- Selenium/FOT AUTOMATION/AdminPanel/src/test/java/runner/TestRunner.java
+++ Selenium/FOT AUTOMATION/AdminPanel/src/test/java/runner/TestRunner.java
@@ -2,15 +2,12 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(
-		features = "src/test/resources/features",
-		glue={"stepDefinitions"},
-		plugin = {"pretty", "html:target/cucumber-report.html"},
-	    monochrome = true,
-	    tags = "@productTransformation3",
-	    dryRun = false
-		)
+
+@CucumberOptions(features = "src/test/resources/features", glue = { "stepDefinitions" }, plugin = { "pretty",
+		"html:target/cucumber-report.html", "rerun:target/failed_scenarios.txt" }, monochrome = true,
+
+		dryRun = false)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-	
+
 }
