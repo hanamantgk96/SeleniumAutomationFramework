3861175b677962947ff19784f365154254148a5e
Akshata Meti <akshata.meti@enhancesys.com>
2026-02-02 14:22:18 +0530

    commiting the failed scenarios rerun class code

--- Selenium/FOT AUTOMATION/AdminPanel/src/test/java/runner/FailedTeseCaseRunner.java
+++ Selenium/FOT AUTOMATION/AdminPanel/src/test/java/runner/FailedTeseCaseRunner.java
@@ -1,5 +1,17 @@
 package runner;
 
-public class FailedTeseCaseRunner {
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+@CucumberOptions(
+		features = "@target/failed_scenarios.txt",
+				glue = "stepDefinitions",
+				plugin = {"pretty","html:target/cucumber-report-failed.html"},
+				monochrome = true
+		)
+
+public class FailedTeseCaseRunner extends AbstractTestNGCucumberTests {
+	
 
 }
