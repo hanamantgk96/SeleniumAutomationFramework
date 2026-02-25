5863a5d61bf3535e7f16023acdacb1002d17edf2
Akshata Meti <akshata.meti@enhancesys.com>
2026-02-25 17:23:02 +0530

    Changes related to  @ProductTransformation11,@UOM7,@SKU9

--- Selenium/FOT AUTOMATION/AdminPanel/src/main/java/utils/BaseTest.java
+++ Selenium/FOT AUTOMATION/AdminPanel/src/main/java/utils/BaseTest.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -29,7 +30,7 @@ public class BaseTest {
 		}
 		else if (browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-			 driver = new ChromeDriver();
+			 driver = new FirefoxDriver();
 			}
 		else if(browser.equalsIgnoreCase("edge")) {
 			WebDriverManager.edgedriver().setup();
